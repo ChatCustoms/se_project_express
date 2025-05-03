36c573e87a08af55b304ffa4f5552b9dd6179aab
ChatCustoms <stephano.chatham@gmail.com>
2025-05-03 10:00:46 -0400

    Commit fixed the Github test errors.

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,5 +19,7 @@ module.exports = {
     ecmaVersion: "latest",
     sourceType: "module",
   },
-  rules: {allow: _id},
+  rules: {
+    "no-underscore-dangle": ["error",{"allow":["_id"]}],
+  },
 };
