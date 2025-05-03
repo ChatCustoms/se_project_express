07b7cf7148fd96d8e5270b655af30e5a4e4f8f72
ChatCustoms <stephano.chatham@gmail.com>
2025-05-03 08:26:37 -0400

    Commit finished most logic and working on the specific tests in Postman for tripleten.

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const mainRouter = require("./routes/index");
 
 const { PORT = 3001 } = process.env;
+
+app.use(express.json());
+app.use((req, res, next) => {
+  req.user = {
+    _id: '68156ae9ed2a440e99d085bd'// paste the _id of the test user created in the previous step
+  };
+  next();
+});
+app.use("/", mainRouter);
 
 mongoose
   .connect("mongodb://127.0.0.1:27017/wtwr_db")
---
+++ controllers/users.js
@@ -0,0 +1,45 @@
+const User = require("../models/User");
+
+const getUsers = (req, res) => {
+  User.find({})
+    .then((users) => {
+      res.status(200).send(users);
+    })
+    .catch((error) => {
+      console.error(error);
+      return res.status(500).send("Internal Server Error");
+    });
+};
+
+const createUser = (req, res) => {
+  const { name, avatar } = req.body;
+
+  User.create({ name, avatar})
+  .then((user) => {
+    res.status(201).send(user);
+  })
+  .catch((error) => {
+    console.error(error);
+    if (error.name === "ValidationError") {
+      return res.status(400).send({ message: "Invalid data" });
+    }
+    return res.status(500).send("Internal Server Error");
+  });
+}
+
+const getUser = (req, res) => {
+  const { userId} = req.params;
+  User.findById(userId)
+  .then((user) => {if (!user) {
+    return res.status(404).send({ message: "User not found" });
+  } res.status(200).send(user)})
+  .catch((error) => {
+    console.error(error);
+   if (error.name === "CastError") {
+      return res.status(400).send({ message: "Invalid user ID" });
+    }
+    return res.status(500).send({ message: "Internal Server Error" });
+  });
+}
+
+module.exports = { getUsers, createUser, getUser };
