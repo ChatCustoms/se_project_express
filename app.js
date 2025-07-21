740bd9a4169d23fc3e50916c76a9fbdea29fa82e
ChatCustoms <stephano.chatham@gmail.com>
2025-07-21 14:27:31 -0400

    commit for Sprint 15 project. I finished updating the backend to read env var.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+require("dotenv").config();
 const mainRouter = require("./routes/index");
 const { handleError, NotFoundError } = require("./utils/errors");
 const errorHandler = require("./middlewares/error-handler");
